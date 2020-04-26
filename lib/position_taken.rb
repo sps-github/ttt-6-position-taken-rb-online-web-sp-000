21c71d003a28f1946ebf3538d633925f6c636659
Sarah Peters <sarahpieters15@gmail.com>
2020-04-26 13:59:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,12 @@
 def position_taken?(board, index)
 end
 
-if array[index_number] == " "
+if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   puts "false"
+elseif board == ["", " ", " ", " ", " ", " ", " ", " ", " "]
+puts "false"
+elseif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "]
+puts "false"
+else board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+puts "true"
 end
