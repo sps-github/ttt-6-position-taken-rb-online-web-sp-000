d265bbcb1c66c54740698d8b38588fd1e880c3bb
Sarah Peters <sarahpieters15@gmail.com>
2020-04-26 14:17:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,3 +3,7 @@ end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = ["0","1", "2", "3", "4", "5", "6", "7", "8"]
+
+if position_taken? == board[" "], index = "0"
+  puts "false"
+end
