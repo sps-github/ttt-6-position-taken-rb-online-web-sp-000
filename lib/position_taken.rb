3a98ec9fd20d511f9f4aa0d4e1caa096a251248d
Sarah Peters <sarahpieters15@gmail.com>
2020-04-26 14:39:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,9 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def position_taken?(board[index])
+def position_taken?(board, index)
 end
 
-if board[0, 1, 2, 3, 4, 5, 6, 7, 8] == "X" || "O"
-  puts "false"
-end
